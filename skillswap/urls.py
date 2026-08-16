a275f0ef41bc211946619090c91252d49395be8b
Kandarp_08 <kandarpupadhyay089@gmail.com>
2026-08-16 18:45:55 +0530

    refactor: Consolidate into single Super Admin Control Center, redirecting /admin/ to /control-center/

--- skillswap/urls.py
+++ skillswap/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from certificates.views import verify_certificate
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Consolidated Single Super Admin Control Center (/admin/ redirects to /control-center/)
+    path('admin/', RedirectView.as_view(url='/control-center/', permanent=False)),
     path('control-center/', include('adminpanel.urls')),
     path('verify/certificate/<str:certificate_id>/', verify_certificate, name='verify_certificate'),
 
